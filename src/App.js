918c1f96d2b21ffa24499bbea2cdafb847ce42c4
Roman Kashigin <r.kashigin@yandex.ru>
2021-03-15 00:16:38 +0300

    Custom hook "firebase auth listener" created

--- src/App.js
+++ src/App.js
@@ -5,9 +5,11 @@ import { Home, Browse, Signin, Signup } from "./pages";
 
 import * as ROUTES from "./constants/routes";
 import { IsUserRedirect, ProtectedRoute } from "./helpers/routes";
+import { useAuthListener } from "./hooks";
 
 export default function App() {
-  const user = null;
+  const { user } = useAuthListener();
+  console.log(user);
 
   return (
     <Router>
