abbfa5f6dc3ca41314d8409748285d4522e06137
Roman Kashigin <r.kashigin@yandex.ru>
2021-03-14 21:27:19 +0300

    Firebase connected

--- src/index.js
+++ src/index.js
@@ -2,14 +2,18 @@ import React from "react";
 import ReactDOM from "react-dom";
 
 import App from "./App";
+import { firebase } from "./lib/firebase.prod";
+import { FirebaseContext } from "./context/firebase";
 
 import "normalize.css";
 import { GlobalStyles } from "./global-styles";
 
 ReactDOM.render(
   <>
-    <GlobalStyles />
-    <App />
+    <FirebaseContext.Provider value={{ firebase }}>
+      <GlobalStyles />
+      <App />
+    </FirebaseContext.Provider>
   </>,
   document.getElementById("root")
 );
---
+++ src/lib/firebase.prod.js
@@ -0,0 +1,17 @@
+import Firebase from "firebase/app";
+import "firebase/firestore";
+import "firebase/auth";
+import { seedDatabase } from "../seed";
+
+const config = {
+  apiKey: "AIzaSyB_0KElLLjmO8TWdrBFzcYk7t5dleKfgV0",
+  authDomain: "netflix-e72e7.firebaseapp.com",
+  projectId: "netflix-e72e7",
+  storageBucket: "netflix-e72e7.appspot.com",
+  messagingSenderId: "240446961368",
+  appId: "1:240446961368:web:a48da71683536fdcb1c9ad",
+};
+
+const firebase = Firebase.initializeApp(config);
+
+export { firebase };
